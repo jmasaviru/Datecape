edf04512ce6cf515aead9d08ff0cda2e2ea52f56
Imari Pickens <imaridp23@gmail.com>
2020-10-26 19:00:18 -0400

    Updating code to script file

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+const submit = document.getElementById("submit");
+const enterName = document.getElementById("enterName");
+
+
+submit.addEventListener("click",saveUserName);
+
+// FUNCTION FOR SAVING & VIEWING USER NAME
+function saveUserName(){
+    userName.style.display = "";
+    var results = "<p>"+ Hello + ' - ' + enterName.value +"</p>"; 
+    localStorage.setItem("showName", results);
+    document.getElementById("userName").innerHTML = localStorage.getItem("showName");  
+}
+
 $("#basicDate").flatpickr({
     enableTime: true,
     dateFormat: "F, d Y H:i",
   }); 
+
+  
