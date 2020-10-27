2ca82c6123c9ab00191053438a8dea89cc9c28aa
Imari Pickens <imaridp23@gmail.com>
2020-10-27 19:00:20 -0400

    Updating code for app.js file

--- app.js
+++ app.js
@@ -4,22 +4,16 @@ var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('#button');
-​
-​
 button.addEventListener('click', function(name){
-fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input.value + "&appid=71d4801a44768bd56efc2fdcaac47597" + "&units=imperial")
-.then(response => response.json())
-.then(data => {
-  var tempValue = data['main']['temp'];
-  var nameValue = data['name'];
-  var descValue = data['weather'][0]['description'];
-​
-  main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
-  input.value ="";
-​
-})
-​
-.catch(err => alert("Wrong city name!"));
-})
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + "&appid=71d4801a44768bd56efc2fdcaac47597" + "&units=imperial")
+        .then(response => response.json())
+        .then(data => {
+            var tempValue = data['main']['temp'];
+            var nameValue = data['name'];
+            var descValue = data['weather']['description'];
+            main.innerHTML = nameValue;
+            desc.innerHTML = "Desc - " + descValue;
+            temp.innerHTML = "Temp - " + tempValue;
+            input.value = "";
+        });
+});
