ca401992dcc7d09c28e0675dc30bb3f82c6431d9
Imari Pickens <imaridp23@gmail.com>
2020-10-29 01:20:30 -0400

    Updating code to app.js file

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
-var button= document.querySelector('#button');
-button.addEventListener('click', function(name){
+var button= document.querySelector('#interestsButton');
+interestsButton.addEventListener('click', function(name){
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + "&appid=71d4801a44768bd56efc2fdcaac47597" + "&units=imperial")
         .then(response => response.json())
         .then(data => {
             var tempValue = data['main']['temp'];
             var nameValue = data['name'];
-            var descValue = data['weather']['description'];
+            var descValue = data['weather'][0]['description'];
             main.innerHTML = nameValue;
             desc.innerHTML = "Desc - " + descValue;
             temp.innerHTML = "Temp - " + tempValue;
