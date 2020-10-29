0e9e76ff33e175201cf007c4c909edfcc2af6121
Imari Pickens <imaridp23@gmail.com>
2020-10-29 01:17:25 -0400

    Updating code to script.js file

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
-const submit = document.getElementById("submit");
-const enterName = document.getElementById("enterName");
+$(document).ready(function(){
 
+    // FUNCTION FOR WHEN GET STARTED BUTTON IS CLICKED IN NAVBAR
+    $("#navButton").click(function(){
+        $("#logoDiv").show();
+        $("#about").hide();
+        $("#getStartedDiv").hide();
+        $("#socialDiv").hide();
+        $("#footerDiv").hide();
+        $("#interestsDiv").hide();
+        $("#signUpDiv").show();
+        $("#weatherDiv").hide();
+        $("#restaurantDiv").hide();
+        $("#userInput2").hide();
 
-submit.addEventListener("click",saveUserName);
-
-// FUNCTION FOR SAVING & VIEWING USER NAME
-function saveUserName(){
-    userName.style.display = "";
-    var results = "<p>"+ Hello + ' - ' + enterName.value +"</p>"; 
-    localStorage.setItem("showName", results);
-    document.getElementById("userName").innerHTML = localStorage.getItem("showName");  
-}
-
-$("#basicDate").flatpickr({
-    enableTime: true,
-    dateFormat: "F, d Y H:i",
-  }); 
+    });
+  
+    // FUNCTION FOR WHEN GET STARTED BUTTON IS CLICKED ON HOME PAGE 
+    $("#getStartedButton").click(function(){
+        $("#about").hide();
+        $("#getStartedDiv").hide();
+        $("#socialDiv").hide();
+        $("#footerDiv").hide();
+        $("#signUpDiv").show();
+    });
+    
+    // FUNCTION FOR WHEN SUBMIT BUTTON IS CLICKED ON SIGN UP PAGE
+    $("#signUpSubmit").click(function(){
+        $("#userInput").append("Hello " + $("#enterName").val()+"! Fill out the survey below to view your results");
+        $("#logoDiv").hide();
+        $("#about").hide();
+        $("#getStartedDiv").hide();
+        $("#socialDiv").hide();
+        $("#footerDiv").hide();
+        $("#signUpDiv").hide();
+        $("#userName").show();
+        $("#interestsDiv").show();
+        $("#basicDate").flatpickr({
+            enableTime: true,
+            dateFormat: "F, d Y H:i",
+        });
+        
+    });
+  
+    // FUNCTION FOR WHEN SUBMIT BUTTON IS CLICKED ON INTERESTS PAGE
+    $("#interestsButton").click(function(){
+        $("#userInput2").append($("#enterName").val()+", here are your results!");
+        $("#logoDiv").hide();
+        $("#about").hide();
+        $("#getStartedDiv").hide();
+        $("#socialDiv").hide();
+        $("#footerDiv").hide();
+        $("#signUpDiv").hide();
+        $("#userName").show();
+        $("#interestsDiv").hide();
+        $("#weatherDiv").show();
+        $("#restaurantDiv").show();
+        $("#userInput").hide();
+    });
+  
+  
+  });
 
   
